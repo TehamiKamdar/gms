5da3061ad0df0edc6c200fcbb2c6498652546a3c
Tehami <tehami@aptechgdn.net>
2024-11-27 23:27:41 +0500

    Starting the Project

---
+++ database/migrations/2024_11_27_175916_create_classes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('trainerId');
+            $table->foreign('trainerId')->on('id')->references('trainers')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('days');
+            $table->time('time');
+            $table->string('capacity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('classes');
+    }
+};
